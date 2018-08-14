686d608e1b7a9154aedfb06baacac96941e33df2
abalmazik <yana.abalmaz@gmail.com>
2018-08-14 21:45:43 +0300

    Create model Product

--- parser_project/shop_parser/models.py
+++ parser_project/shop_parser/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    brand = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.CharField(max_length=250)
+    currency = models.CharField(max_length=5)
+    size = models.CharField(max_length=5)
+    color = models.CharField(max_length=250)
+    image = models.URLField()
